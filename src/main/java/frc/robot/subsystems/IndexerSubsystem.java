3bc7f3adfefb10cc2a71a3a1f63bb663bd0195c1
maddox-Nightbots <maddox@nightbots.org>
2026-07-14 14:51:56 -0600

    Finished preping indexer subsystem

--- src/main/java/frc/robot/subsystems/IndexerSubsystem.java
+++ src/main/java/frc/robot/subsystems/IndexerSubsystem.java
@@ -15,53 +15,53 @@ import com.revrobotics.spark.config.SparkMaxConfig;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants.IntakeConstants;
+
 /*import frc.robot.LimelightHelpers;*/
 
-public class IntakeSubsystem extends SubsystemBase {
+public class IndexerSubsystem extends SubsystemBase {
 
     //create motors
-    private final TalonFX intakeMotor;
-    private final SparkMax armMotor;
+    private final TalonFX IndexerMotor;
+    private final SparkMax FeederMotor;
 
 
-    private final VelocityVoltage intakevelocityRequest = new VelocityVoltage(0);
+    private final VelocityVoltage IndexervelocityRequest = new VelocityVoltage(0);
 
-    private final SparkClosedLoopController intakepidController;
+    private final SparkClosedLoopController feederpidController;
 
 
-    public IntakeSubsystem() {
+    public IndexerSubsystem() {
 
         //config motors
-        intakeMotor = new TalonFX(5);
-        armMotor = new SparkMax(6, MotorType.kBrushless);
+        IndexerMotor = new TalonFX(5);
+        FeederMotor = new SparkMax(6, MotorType.kBrushless);
 
-        //config arm motor
-        SparkMaxConfig armConfig = new SparkMaxConfig();
+        //config Feeder motor
+        SparkMaxConfig feederConfig = new SparkMaxConfig();
 
-        armConfig.inverted(true);
-        armConfig.idleMode(SparkMaxConfig.IdleMode.kBrake);
+        feederConfig.inverted(true);
+        feederConfig.idleMode(SparkMaxConfig.IdleMode.kBrake);
 
-        armConfig.smartCurrentLimit(40);
-        armConfig.closedLoop.p(0.0002).i(0.000001).d(0.0004);
-        armMotor.configure(armConfig, kResetSafeParameters, kPersistParameters);
+        feederConfig.smartCurrentLimit(40);
+        feederConfig.closedLoop.p(0.0002).i(0.000001).d(0.0004);
+        FeederMotor.configure(feederConfig, kResetSafeParameters, kPersistParameters);
 
-        TalonFXConfiguration intakeConfig = new TalonFXConfiguration();
+        TalonFXConfiguration IndexerConfig = new TalonFXConfiguration();
           // Match the 40A baseline to protect your robot battery during high loads
-        intakeConfig.CurrentLimits.SupplyCurrentLimit = 40.0;
-        intakeConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
-        intakeConfig.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+        IndexerConfig.CurrentLimits.SupplyCurrentLimit = 40.0;
+        IndexerConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
+        IndexerConfig.MotorOutput.NeutralMode = NeutralModeValue.Brake;
 
 
-        intakeConfig.Slot0.kP = 0.11;  // Proportional gain (corrects error)
-        intakeConfig.Slot0.kV = 0.12;  // Feedforward gain (scales voltage to expected velocity)
-        intakeConfig.Slot0.kI = 0.0;   // Integral gain (rarely needed for FRC velocity)
-        intakeConfig.Slot0.kD = 0.0;   // Derivative gain (helps reduce overshoot)
+        IndexerConfig.Slot0.kP = 0.11;  // Proportional gain (corrects error)
+        IndexerConfig.Slot0.kV = 0.12;  // Feedforward gain (scales voltage to expected velocity)
+        IndexerConfig.Slot0.kI = 0.0;   // Integral gain (rarely needed for FRC velocity)
+        IndexerConfig.Slot0.kD = 0.0;   // Derivative gain (helps reduce overshoot)
 
         // Apply configs to Kraken
-        intakeMotor.getConfigurator().apply(intakeConfig);
+        IndexerMotor.getConfigurator().apply(IndexerConfig);
 
-        intakepidController = armMotor.getClosedLoopController();
+        feederpidController = FeederMotor.getClosedLoopController();
 
     }
 
@@ -69,49 +69,31 @@ public class IntakeSubsystem extends SubsystemBase {
      * Drives the Kraken at a specific velocity target.
      * @param targetRPM The desired engine speed in Revolutions Per Minute (Max ~6000 RPM)
      */
-    public void setVelocityRPM(double targetRPM) {
+    public void setIndexerVelocityRPM(double targetRPM) {
         // Convert RPM to Rotations Per Second (RPS)
         double targetRPS = targetRPM / 60.0;
 
         // Use the request object to smoothly command the motor
-        intakeMotor.setControl(intakevelocityRequest.withVelocity(targetRPS));
+        IndexerMotor.setControl(IndexervelocityRequest.withVelocity(targetRPS));
     }
 
-    public void setIntakePosition(double position) {
-        intakepidController.setSetpoint(position * IntakeConstants.gearRatio, ControlType.kPosition);
+    public void setFeederVelocityRPM(double targetRPM){
+        feederpidController.setSetpoint(targetRPM, ControlType.kVelocity);
     }
 
-    public double getIntakePosition() {
-        return intakepidController.getSetpoint() / IntakeConstants.gearRatio;
-    }
-
-    public Command SpinIntakeWheels(){
+    public Command SpinIndexer(){
         return this.run(() -> {
-            this.setVelocityRPM(2000);
+            this.setIndexerVelocityRPM(2000);
         });
     }
 
-    public Command IntakeDown() {
-        return this.runOnce(() -> {
-        this.setIntakePosition(IntakeConstants.IntakeDownPosition);
-    });
+    public Command SpinFeeder(){
+        return this.run(() -> {
+            this.setFeederVelocityRPM(2000);
+        });
     }
 
-    public Command IntakeUp() {
-        return this.runOnce(() -> {
-        this.setIntakePosition(IntakeConstants.IntakeUpPosition);
-    });
-    }
-
-    public Command IntakeUpDown() {
-        return Commands.sequence(
-            this.runOnce(() -> this.setIntakePosition(IntakeConstants.IntakeDownPosition*3/4)).until(() -> this.getIntakePosition() == IntakeConstants.IntakeDownPosition*3/4),
-        
-            this.runOnce(() -> this.setIntakePosition(IntakeConstants.IntakeDownPosition/4)).until(() -> this.getIntakePosition() == IntakeConstants.IntakeDownPosition/4)
-        ).repeatedly();
-    }
-
-    public Command Intaking(){
-        return Commands.sequence(this.runOnce(() -> this.IntakeDown()), this.run(() -> this.SpinIntakeWheels()));
+    public Command SpinIndexerShoot(){
+        return Commands.parallel(SpinFeeder(),SpinIndexer());
     }
 }
