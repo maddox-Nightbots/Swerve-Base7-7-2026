1b0cbdcc0cfbf86cac9d95ac55af22b971e26ff2
maddox-Nightbots <maddox@nightbots.org>
2026-07-23 13:50:46 -0600

    hopefully everything is synced now

--- src/main/java/frc/robot/commands/turretAim.java
+++ src/main/java/frc/robot/commands/turretAim.java
@@ -8,6 +8,8 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.TurretSubsystem;
 import org.photonvision.targeting.PhotonTrackedTarget;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 
 
 public class turretAim extends Command{
@@ -18,6 +20,16 @@ public class turretAim extends Command{
         addRequirements(turret);
         this.turret = turret;
         this.targets = targets;
-    
+    }
+
+    private double getHubYaw(double turretAngle, List<PhotonTrackedTarget> targetstoAim){
+        double angle = 0.0;
+        for (var target: targetstoAim){
+            if(target.getFiducialId() == (alliance.get() == Alliance.Blue ? 1:0)){
+                angle = target.getYaw();
+            }
+        }
+        double targetAngle = turretAngle + angle;
+        return targetAngle;
     }
 }
